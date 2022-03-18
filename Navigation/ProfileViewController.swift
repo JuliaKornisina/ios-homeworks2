e5fbc60c9c0d9c7f3491e79145e82e95ef079f19
JuliaKornisina <julia20m@mail.ru>
2022-03-18 10:23:29 +0300

    task completion

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Юлия Малюгина on 18.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
